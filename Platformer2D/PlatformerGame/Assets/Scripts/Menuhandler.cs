d3060b0866a36b5909020e98657c733906896ab7
paarii <101456989@student.swin.edu.au>
2019-06-23 18:33:46 +1000

    Demo
    Shitty Demo

---
+++ Platformer2D/PlatformerGame/Assets/Scripts/Menuhandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Menuhandler : MonoBehaviour
+{
+    
+    public bool Jumping = false;
+    public bool Moving = false;
+    public Button button;
+    public GameObject selctionMenu;
+    public void OnJumpClick()
+    {
+        Jumping = true;
+        button.interactable = false;
+        
+    }
+    public void OnMoveClick()
+    {
+        Moving = true;
+    }
+
+    public void OnContinueClick()
+    {
+        selctionMenu.SetActive(false);
+    }
+
+}
---
+++ Platformer2D/PlatformerGame/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller_Ref;
+
+    //Movement Varaiables
+    public float moveSpeed = 40f;
+    private float horizontalAxis;
+    public bool isJump = false;
+    public Menuhandler playerHandle;
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHandle.Moving == true)
+        {
+            horizontalAxis = Input.GetAxisRaw("Horizontal") * moveSpeed;
+            animator.SetFloat("Speed",Mathf.Abs(horizontalAxis));
+        }
+        if(playerHandle.Jumping == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetBool("isJumping", true);
+                isJump = true;
+                
+            }
+            
+        }
+       
+       
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    private void FixedUpdate()
+    {
+        controller_Ref.Move(horizontalAxis * Time.fixedDeltaTime, false, isJump);
+        isJump = false;
+        
+
+    }
+}
